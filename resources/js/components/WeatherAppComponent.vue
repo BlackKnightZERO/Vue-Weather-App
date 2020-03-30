bd80f8e0862dc5d75d867aff258ebbad0edb8247
Arif Faysal <ayon.arif.10@gmail.com>
2020-03-30 21:14:34 +0600

    first

---
+++ resources/js/components/WeatherAppComponent.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="text-white mb-8">
+       <div class="places-input text-grey-800">
+        <input type="text" class="w-full">
+       </div>
+       <div class="weather-container font-sans w-128 max-w-lg overflow-hidden bg-grey-900 shadow-lg mt-4">
+        <div class="current-weather">
+            
+        </div>
+       </div> <!-- end weather container -->
+   </div>
+</template>
+
+<script>
+    export default {
+    mounted() {
+    console.log('Component mounted.')
+    }
+}
+</script>
